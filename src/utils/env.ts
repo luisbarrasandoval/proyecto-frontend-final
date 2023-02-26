6de5251e5fdb8069bf69f8e9e088d5a5696d50be
Luis Barra <contacto@luisbarra.tech>
2023-02-26 12:43:48 -0300

    update build

--- src/utils/env.ts
+++ src/utils/env.ts
@@ -1,5 +1,8 @@
-export const BACK_URL: string = import.meta.env.VITE_BACK_URL || "";
-export const FRONT_URL: string = import.meta.env.VITE_FRONT_URL || "";
+export const BACK_URL: string = import.meta.env.VITE_BACK_URL || "http://ec2-34-226-245-123.compute-1.amazonaws.com";
+export const FRONT_URL: string = import.meta.env.VITE_FRONT_URL || "http://ec2-34-226-245-123.compute-1.amazonaws.com:3000";
+
+console.log(BACK_URL)
+console.log(FRONT_URL)
 
 if (!BACK_URL) {
   throw new Error('BACK_URL is not defined');
